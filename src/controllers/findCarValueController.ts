92913eea66bfa73453a7ef803244b93454454c69
mind-ll-matter <aj.git001@gmail.com>
2023-05-30 10:52:08 +1200

    edited controller to see if it fixes published api issue

--- src/controllers/findCarValueController.ts
+++ src/controllers/findCarValueController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express'
 import * as carValueService from '../services/findCarValueService'
 
 export const findCarValue = (req: Request, res: Response) => {
-  const model = req.body.model
-  const year = req.body.year
+  const model: string | number = req.body.model
+  const year: number = req.body.year
 
   const carValueObj = {
     carValue: carValueService.findCarValue(model, year),
